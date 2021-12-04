722077242fc7c04da8ab2b31ebe77935693aff47
marpme <marpme@users.noreply.github.com>
2021-12-04 14:24:10 +0100

    reorg imports for tests

--- src/task1/test.rs
+++ src/task1/test.rs
@@ -1,21 +1,21 @@
-use crate::task1::entry;
+use crate::task1::entry::{task1, task1_2};
 
 #[test]
 fn basic() {
-   assert_eq!(entry::task1("./src/task1/input/basic_task1".to_string()), 7);
+   assert_eq!(task1("./src/task1/input/basic_task1".to_string()), 7);
 }
 
 #[test]
 fn challenge() {
-   assert_eq!(entry::task1("./src/task1/input/challenge_task1".to_string()), 1121);
+   assert_eq!(task1("./src/task1/input/challenge_task1".to_string()), 1121);
 }
 
 #[test]
 fn basic2() {
-   assert_eq!(entry::task1_2("./src/task1/input/basic_task1".to_string()), 5);
+   assert_eq!(task1_2("./src/task1/input/basic_task1".to_string()), 5);
 }
 
 #[test]
 fn challenge2() {
-   assert_eq!(entry::task1_2("./src/task1/input/challenge_task1_2".to_string()), 1065);
+   assert_eq!(task1_2("./src/task1/input/challenge_task1_2".to_string()), 1065);
 }
